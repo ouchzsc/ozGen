494a9f94c29b8a7e2de5ac62727f4644d8736cb7
zhangao <ouchzsc@gmail.com>
2020-09-14 23:07:05 +0800

    目录调整。添加启动参数

--- ozGen/src/Main.java
+++
@@ -1,9 +0,0 @@
-import gen.*;
-import logger.LoggerWrap;
-
-public class Main {
-    public static void main(String[] args) {
-        LoggerWrap.instance.init();
-        cfgMgr.instance.gen("data");
-    }
-}
